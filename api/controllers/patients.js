833780823c975b7e61fb2479720ae13c494c701b
João Correia <correia.jafonso@gmail.com>
2021-11-05 15:04:15 +0000

    Refactoring controller arg

--- api/controllers/patients.js
+++ api/controllers/patients.js
@@ -4,7 +4,7 @@ const patientSchema = require('../models/patient')
 exports.getAllPatients = (req, res) => {
 
  
-   connection.query('CALL Get_All_Patients()', (err, rows) => {
+   connection.query('CALL Get_All_Patients()', (err, [rows]) => {
             if(err) throw err 
             else { 
                 res.send(rows)
@@ -16,7 +16,7 @@ exports.getAllPatients = (req, res) => {
 
 exports.getAllAdmittedPatients = (req, res) => {
  
-    connection.query('CALL Get_All_Patients_Admitted()', (err, rows) => {
+    connection.query('CALL Get_All_Patients_Admitted()', (err, [rows]) => {
         if(err) throw err
         else {
             res.send(rows)
