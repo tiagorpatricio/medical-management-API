8aa9dd89ff6dfdde401929dd049b105f682288ff
Tiago Patricio <69849680+Tiago-Patricio@users.noreply.github.com>
2021-11-06 13:53:52 +0000

    Fixes api structure

--- api/controllers/patients.js
+++ api/controllers/patients.js
@@ -1,26 +1,9 @@
-const connection = require('../utils/connection')
-const patientSchema = require('../models/patient')
+const patientsService = require("../services/patients");
 
 exports.getAllPatients = (req, res) => {
-
- 
-   connection.query('CALL Get_All_Patients()', (err, rows) => {
-            if(err) throw err 
-            else { 
-                res.send(rows)
-            }
-        })
-       
-    
-}
+  res.send(patientsService.getAllPatientsServ());
+};
 
 exports.getAllAdmittedPatients = (req, res) => {
- 
-    connection.query('CALL Get_All_Patients_Admitted()', (err, rows) => {
-        if(err) throw err
-        else {
-            res.send(rows)
-        }
-    })
- 
-}
+  res.send(patientsService.getAllAdmittedPatientsServ());
+};
