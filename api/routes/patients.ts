dd8fae21bf9e21cfa6a430deb9d2248129a2a162
Tiago Patricio <69849680+Tiago-Patricio@users.noreply.github.com>
2022-01-01 22:57:15 +0000

    Added create and update patient

--- api/routes/patients.ts
+++ api/routes/patients.ts
@@ -2,9 +2,23 @@ const express = require("express");
 const router = express.Router();
 const patientsController = require("../controllers/patients");
 
-router.get("/all", patientsController.getAllPatients);
-router.get("/admitted", patientsController.getAllAdmittedPatients);
-router.get("/:patientId", patientsController.getPatientById);
+router.get("/all", (req: any, res: any) => {
+  patientsController.getAllPatients(req, res);
+});
+router.get("/admitted", (req: any, res: any) => {
+  patientsController.getAllAdmittedPatients(req, res);
+});
+router.get("/:patientId", (req: any, res: any) =>
+  patientsController.getPatientById(req, res)
+);
+router.post("/createNewPatient", (req: any, res: any) => {
+  patientsController.createNewPatient(req, res);
+});
+
+router.put("/updatePatient/:patientId", (req: any, res: any) => {
+  patientsController.updatePatient(req, res);
+});
+
 module.exports = router;
 
 export {};
